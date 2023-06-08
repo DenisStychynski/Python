c32b631461c4fcdc8f76483d5f1b498cd02071ed
Denis <stychynskidenis@gmail.com>
2023-06-08 15:30:30 +0300

    Выполнили семинар

---
+++ Seminar5.py
@@ -0,0 +1,30 @@
+# import CustomFuncs
+
+# print(CustomFuncs.LastFibo(7))
+
+#/////////////////////////////////////
+
+# a = [5, 2, 3, 1, 2, 4, 5, 3]
+
+# for item in a:
+#     if item == max(a):
+#         item = min(a)
+#     print(item)
+
+# ///////////////////////////////////
+
+# number = int (input("Введите число"))
+# def isSimple(number):
+#     if number > 2 and number % 2 !=0 :
+#         for i in range (3, number // 2):
+#             if number % i == 0:
+#                 return False
+#         return True
+#     return False
+        
+# ///////////////////////////////////
+
+# import CustomFuncs
+
+# number = int(input("input a number: "))
+# CustomFuncs.reverseRange(number)
